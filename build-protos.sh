af76bf51238e20903b2d678e2b03c08cff7f9339
Taumer Anabtawi <taumer.anabtawi@senseye.co>
2020-02-24 18:23:16 -0600

    creating public protos sep

--- build-protos.sh
+++ build-protos.sh
@@ -8,8 +8,13 @@ if ! [ -x "$PYTHON_BIN" ]; then
 fi
 echo "Found Python interpreter at: $PYTHON_BIN"
 
-OUTPUT_PATH=./eucalyptus_protos/python_protos
-INPUT_PATH=./eucalyptus_protos/protobuf
+OUTPUT_PATH=./senseye_api_protos/python
+INPUT_PATH=./senseye_api_protos/protobuf
+
+declare -a PUB_PROTOS
+PUB_PROTOS=('gateway_service.proto'
+            'status.proto'
+            'common.proto')
 
 echo "Input path: $INPUT_PATH"
 echo "Output path: $OUTPUT_PATH"
@@ -20,7 +25,17 @@ mkdir -p $OUTPUT_PATH;
 
 # Generate protoc
 echo "Building Protos..."
-$PYTHON_BIN -m grpc_tools.protoc -I$INPUT_PATH --python_out=$OUTPUT_PATH --grpc_python_out=$OUTPUT_PATH $INPUT_PATH/*.proto
+
+# If BUILD_PUBLIC_PROTOS is not set, build all
+if [[ -z "${BUILD_PUBLIC_PROTOS}" ]]; then
+    $PYTHON_BIN -m grpc_tools.protoc -I$INPUT_PATH --python_out=$OUTPUT_PATH --grpc_python_out=$OUTPUT_PATH $INPUT_PATH/*.proto
+else
+    for proto in "${PUB_PROTOS[@]}"; do
+       PROTO=${proto}
+       PUB_PROTO="$INPUT_PATH/$PROTO"
+       $PYTHON_BIN -m grpc_tools.protoc -I$INPUT_PATH --python_out=$OUTPUT_PATH --grpc_python_out=$OUTPUT_PATH $PUB_PROTO
+    done
+fi
 
 # Replace with relative imports
 if [ `uname -s` == 'Darwin' ]; then
